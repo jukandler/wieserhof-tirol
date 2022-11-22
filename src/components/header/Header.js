6c6e1091e82fdc0f70f0d14a5c7369b4f19207b6
Julia Kandler <julia.kandler@sz.de>
2022-11-22 09:09:30 +0100

    add moving header image animation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,11 @@
 import logo from './wieserhof_logo.jpg';
-import hero_image from './balkon_view.jpg';
-import hero_image_mob from './balkon_view-mobile.jpg';
 
 import './header.css';
 
 function Header() {
     return (
         <header>
-            <picture>
-                <source media="(max-width: 799px)" srcSet={hero_image_mob} />
-                <img className="hero-image" src={hero_image} alt="Aussicht vom Balkon ins Tal" />
-            </picture>
+            <div className="hero-image"></div>
             <img className="wieserhof-logo" src={logo} alt="wieserhof logo"></img>
         </header>
     )
