7abe6dd736063daf33f83687b197fccfe800fbdf
Julia Kandler <julia.kandler@sz.de>
2022-11-25 21:59:54 +0100

    scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Home from './pages/Home';
 
 import './App.css';
 import Imprint from "./pages/Imprint";
+import ScrollToTop from "./helpers/scrollToTop";
 
 function App() {
   return (
     <div className="App">
       <HashRouter basename={process.env.PUBLIC_URL}>
+        <ScrollToTop />
         <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/impressum" element={<Imprint/>} />
