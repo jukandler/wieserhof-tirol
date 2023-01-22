c704db8bf2f794957537889e5fea232325763101
Julia Kandler <julia.kandler@sz.de>
2023-01-22 18:32:52 +0100

    add basic navigation menu styling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,27 @@
 import './navigation.css';
 import MenuIcon from "../../assets/icons/menu.svg";
-
+import CloseIcon from "../../assets/icons/close.svg";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../Logo';
 
 function Navigation() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => setIsOpen(!isOpen);
+
     return (
         <nav>
-            <div className="icon-close">
-                <img src={MenuIcon} alt="menü schließen"/>
+            <div className="navbar-icon" onClick={handleClick}>
+                <img src={isOpen ? CloseIcon : MenuIcon} alt="menü schließen"/>
             </div>
+            { isOpen && <div className="navigation-menu">
+                <Logo />
+                <div className="navigation-menu__links">
+                    <Link to="/" onClick={handleClick}>Startseite</Link>
+                </div>
+            </div>}
         </nav>
     )
 }
