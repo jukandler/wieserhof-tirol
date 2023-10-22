7ad52532d0444cb44dc0a889281463a7ad11528a
Julia Kandler <julia.kandler@sz.de>
2023-10-22 16:47:25 +0200

    remove console.log

--- src/components/consent/ConsentBanner.js
+++ src/components/consent/ConsentBanner.js
@@ -7,7 +7,6 @@ function ConsentBanner({setConsent}) {
 
     useEffect(() => {
         setConsent(getCookieConsentValue("CookieConsent"))
-        console.log("init consent value:", getCookieConsentValue("CookieConsent"))
     }, [setConsent]);
 
     return (
@@ -17,10 +16,7 @@ function ConsentBanner({setConsent}) {
                 buttonText="Akzeptieren"
                 style={{ background: "#C0AB96" }}
                 buttonStyle={{ background: "#EEE9E2", fontSize: "20px", borderRadius: "5px", padding: "10px" }}
-                onAccept={() => {
-                    console.log("accept")
-                    setConsent(true)
-                }}
+                onAccept={() => setConsent(true)}
             >
                 Dieses Buchungssystem verwendet Cookies für statistische Analysezwecke. Wir benötigen daher Ihre Einwilligung.{" "}
             </CookieConsent>
