b8bb832b67957cd8d252c94a790a9fe868d27037
Julia Kandler <julia.kandler@sz.de>
2025-01-13 17:28:29 +0100

    adjust free slots

--- src/components/freeSlots/FreeSlots.jsx
+++ src/components/freeSlots/FreeSlots.jsx
@@ -2,11 +2,9 @@ import "./freeslots.css";
 
 function FreeSlots() {
     return <div className="free-slots">
-        <h4>Kurzfristig verfügbar:</h4>
+        <h4>Letzte Chance!</h4>
         <div className="date-container">
-            <div>06. - 10.1.</div>
             <div>12. - 17.1.</div>
-            <div>28. - 31.1.</div>
             <div>06. - 14.2.</div>
             <div>07. - 15.3.</div>
         </div>
