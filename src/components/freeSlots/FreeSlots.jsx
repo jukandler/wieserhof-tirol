7bc17a2ef0e4e04476c6c35bcda9230a873610b4
Julia Kandler <julia.kandler@sz.de>
2025-01-13 18:05:19 +0100

    get free slots from google spreadsheet

--- src/components/freeSlots/FreeSlots.jsx
+++ src/components/freeSlots/FreeSlots.jsx
@@ -1,14 +1,46 @@
+import { useEffect, useState } from "react";
 import "./freeslots.css";
 
 function FreeSlots() {
-    return <div className="free-slots">
-        <h4>Letzte Chance!</h4>
-        <div className="date-container">
-            <div>12. - 17.1.</div>
-            <div>06. - 14.2.</div>
-            <div>07. - 15.3.</div>
+    const [slots, setSlots] = useState(null);
+
+    useEffect(() => {
+        const apiKey = process.env.REACT_APP_SPREADSHEET_API_KEY;
+        const spreadsheetId = process.env.REACT_APP_SPREADSHEET_ID;
+        const range = 'Sheet1!A2:A6';
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`
+                );
+                const result = await response.json();
+
+                if (result.values) {
+                    setSlots(result.values);
+                } else {
+                    console.error('Keine Daten gefunden.');
+                }
+            } catch (error) {
+                console.error('Fehler beim Abrufen der Daten:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (!slots) {
+        return undefined;
+    }
+
+    return (
+        <div className="free-slots">
+            <h4>Letzte Chance!</h4>
+            <div className="date-container">
+                {slots.map((slot, i) => <div key={i}>{slot}</div>)}
+            </div>
         </div>
-    </div>
+    )
 }
 
 export default FreeSlots;
